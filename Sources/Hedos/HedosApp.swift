ab85075fd7c1de34cb32361208cfb2de90e0a42e
theiskaa <me@theiskaa.com>
2026-07-05 23:24:07 +0400

    fix(app): activate app and bring window to front on launch

--- Sources/Hedos/HedosApp.swift
+++ Sources/Hedos/HedosApp.swift
@@ -1,11 +1,19 @@
+import AppKit
 import HedosKernel
 import SwiftUI
 
 @main
 struct HedosApp: App {
+    init() {
+        NSApplication.shared.setActivationPolicy(.regular)
+    }
+
     var body: some Scene {
         WindowGroup("Hedos") {
             LibraryView()
+                .onAppear {
+                    NSApp.activate(ignoringOtherApps: true)
+                }
         }
     }
 }
